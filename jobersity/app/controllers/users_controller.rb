c5f6f8ab2d86de6c8a4a2153bdc6aff55d575e9e
Pato <pevargas@valdosta.edu>
2016-08-15 19:38:10 -0400

    new user working

--- jobersity/app/controllers/users_controller.rb
+++ jobersity/app/controllers/users_controller.rb
@@ -4,8 +4,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    #return all the user by name
-   @users = User.order(:name)
+    @users = User.order(:name)
   end
 
   # GET /users/1
@@ -30,11 +29,13 @@ class UsersController < ApplicationController
     respond_to do |format|
       if @user.save
         format.html { redirect_to users_url,
-        notice: "User #{@user.name} was successfully created." }
-        format.json { render :show, status: :created, location: @user }
+          notice: "User #{@user.name} was successfully created." }
+        format.json { render action: 'show',
+          status: :created, location: @user }
       else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        format.html { render action: 'new' }
+        format.json { render json: @user.errors,
+          status: :unprocessable_entity }
       end
     end
   end
@@ -44,13 +45,13 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-       if @user.update(user_params)
         format.html { redirect_to users_url,
-notice: "User #{@user.name} was successfully updated." }
-        format.json { render :show, status: :ok, location: @user }
+          notice: "User #{@user.name} was successfully updated." }
+        format.json { head :no_content }
       else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        format.html { render action: 'edit' }
+        format.json { render json: @user.errors,
+          status: :unprocessable_entity }
       end
     end
   end
@@ -60,7 +61,7 @@ notice: "User #{@user.name} was successfully updated." }
   def destroy
     @user.destroy
     respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      format.html { redirect_to users_url }
       format.json { head :no_content }
     end
   end
