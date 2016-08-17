129d6a7862f01204386b23266396e9cce29d6ed9
pevargasg <pevargas@valdosta.edu>
2016-08-17 10:42:43 -0400

    modal logIn working

--- jobersity/app/controllers/inicio_controller.rb
+++ jobersity/app/controllers/inicio_controller.rb
@@ -2,4 +2,12 @@ class InicioController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  def new
+  	@user = User.new
+  	@user = User.find(params[:goal_id])
+  end
+  def index
+  	@user = User.new
+  end
+
 end
