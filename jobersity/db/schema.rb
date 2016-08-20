b1da74c318a34e2917cdd7a2c546ab0b467204b0
Matthew Cliatt <mtcliatt@valdosta.edu>
2016-08-20 15:53:43 -0400

    rename users name column to first_name

--- jobersity/db/schema.rb
+++ jobersity/db/schema.rb
@@ -11,10 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160817134208) do
+ActiveRecord::Schema.define(version: 20160820195128) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
+    t.string   "first_name"
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
