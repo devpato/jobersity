3549fdb6c65ad776372704969fb831930f62f9a4
Matthew Cliatt <mtcliatt@valdosta.edu>
2016-08-20 15:57:38 -0400

    fixes name validation in user model

--- jobersity/app/models/user.rb
+++ jobersity/app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-	validates :name, presence: true
+	validates :first_name, presence: true
+	validates :last_name, presence: true
 	validates :email, presence: true, uniqueness: true
 
 	has_secure_password
