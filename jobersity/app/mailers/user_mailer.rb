42bf7f60b8c4a44c17927bc22acd07cece6b16e8
Matthew Cliatt <mtcliatt@valdosta.edu>
2016-08-20 13:34:19 -0400

    removes test email method

--- jobersity/app/mailers/user_mailer.rb
+++ jobersity/app/mailers/user_mailer.rb
@@ -2,21 +2,16 @@ class UserMailer < ApplicationMailer
 	default from: 'matt@wearetamo.com'
 
 	# TODO:
-	# 
+	#
 	# 1 - Add a plain-text version of every html email
-	# 
+	#
 	# 2 - Add a physical mailing address to comply with the SPAM laws
 	#
 	# 3 -
-	#
 
 	def welcome_email(user)
 		@user = user
 		mail(to: @user.email, subject: 'Welcome to Jobersity!')
-	end
-
-	def welcome_email
-		mail(to: 'tamo@mailinator.com', subject: 'Welcome to Jobersity!')
 	end
 
 end
